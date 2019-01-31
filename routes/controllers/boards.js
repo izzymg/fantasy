7f4df8c0429fe5895a1521229a4a046d38990d2a
izzymg <izzymhg@gmail.com>
2019-01-31 17:22:02 +1300

    Bugfix

--- routes/controllers/boards.js
+++ routes/controllers/boards.js
@@ -3,7 +3,7 @@ const templatesConfig = require("../../config/templates");
 
 exports.render = async (ctx) => {
     try {
-        const boards = await db.fetchAll("SEELCT url, title, about, sfw, createdAt FROM boards");
+        const boards = await db.fetchAll("SELECT url, title, about, sfw, createdAt FROM boards");
         await ctx.render("boards", { title: templatesConfig.titles.boards, boards });
     } catch(error) {
         return ctx.throw(500, error);
