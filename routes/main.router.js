bc19c23e70e676c266449202e7d59d40393f1a4c
izzymg <izzymhg@gmail.com>
2019-02-03 19:04:06 +1300

    Added redirects to router

--- routes/main.router.js
+++ routes/main.router.js
@@ -8,6 +8,7 @@ const thread = require("./controllers/thread");
 const files = require("./controllers/files");
 const notfound = require("./controllers/notfound");
 
+// Boards are cached to prevent excess DB queries
 function setup() {
     boards.genCache();
 }
@@ -22,7 +23,11 @@ router.get("/boards", async ctx => {
 router.get("/boards/:board", async ctx => {
     await ctx.redirect(`/boards/${ctx.params.board}/`);
 });
+router.get("/boards/:board/threads/:thread/", async ctx => {
+    await ctx.redirect(`/boards/${ctx.params.board}/threads/${ctx.params.thread}`);
+});
 
+// Render boards list and board catalog
 router.get("/boards/", boards.render);
 router.get("/boards/:board/", boards.checkBoard, catalog.render);
 
@@ -35,10 +40,13 @@ router.post("/boards/:board/", boards.checkBoard, parseRequests.parseThread, par
     }
 );
 
+// Thread
 router.get("/boards/:board/threads/:thread", thread.render);
 
+// Serve files
 router.get("/files/:filename", files.render);
 
+// Fallthroughs
 router.get("*", notfound.render);
 router.get("/404", notfound.render);
 
