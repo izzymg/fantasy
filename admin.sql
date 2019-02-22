34fa3a19728ab11b58d6ea2b2fedcd9c60c98b5f
izzymg <izzymhg@gmail.com>
2019-02-22 20:33:26 +1300

    Fixed SQL files for user setup

---
+++ admin.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(username, password) VALUES (
+    "admin", 
+    "$2y$12$Hs44cH8Xh/5JuzLf.E25fuBby4XjA7wZNBg0X34hHMuXD6awEUz6u"
+);
+
+INSERT INTO administrators(username) VALUES ("admin");
--- setup.sql
+++ setup.sql
@@ -9,14 +9,6 @@ CREATE TABLE IF NOT EXISTS boards (
     createdAt datetime DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO boards (
-    url, title, about,
-    sfw, bumpLimit, maxThreads, cooldown
-    ) VALUES (
-    "new", "Setup board", "Welcome",
-    true, 300, 25, 30
-);
-
 CREATE TABLE IF NOT EXISTS posts (
     uid integer PRIMARY KEY AUTO_INCREMENT,
     boardUrl varchar(15) NOT NULL,
@@ -34,8 +26,8 @@ CREATE TABLE IF NOT EXISTS posts (
     UNIQUE KEY boarduid (boardUrl, postId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE INDEX postUid ON posts (uid);
-CREATE INDEX postId ON posts (postId);
+CREATE INDEX IF NOT EXISTS postUid ON posts (uid);
+CREATE INDEX IF NOT EXISTS postId ON posts (postId);
 
 CREATE TABLE IF NOT EXISTS boardids (
     boardUrl varchar(15) NOT NULL,
@@ -45,7 +37,7 @@ CREATE TABLE IF NOT EXISTS boardids (
         ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TRIGGER updateboardids
+CREATE TRIGGER IF NOT EXISTS updateboardids
     AFTER INSERT ON boards
     FOR EACH ROW
     INSERT INTO boardids SET boardUrl = new.url, id = 1;
@@ -64,3 +56,27 @@ CREATE TABLE IF NOT EXISTS files (
         ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS users (
+    username varchar(100) PRIMARY KEY,
+    password text,
+    createdAt datetime DEFAULT CURRENT_TIMESTAMP
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS administrators (
+    username varchar(100) UNIQUE,
+    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT adminuser
+        FOREIGN KEY (username) REFERENCES users (username)
+        ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS moderators (
+    username varchar(100) NOT NULL,
+    boardUrl varchar(15) NOT NULL,
+    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT moduser
+        FOREIGN KEY (username) REFERENCES users (username)
+        ON DELETE CASCADE,
+    CONSTRAINT modboard
+        FOREIGN KEY (boardUrl) REFERENCES boards (url)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
